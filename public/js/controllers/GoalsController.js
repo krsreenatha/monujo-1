c5985e089e974bc5173827805c1be3645a6b18be
Rodrigo Muniz <muniz95@bsd.com.br>
2016-09-26 16:13:15 +0000

    Goals CRUD finished

--- public/js/controllers/GoalsController.js
+++ public/js/controllers/GoalsController.js
@@ -1,5 +1,13 @@
 var GoalsController = function($scope,Goal){
+	$scope.$on('$viewContentLoaded', function() {
+		$('.dropdown').dropdown()
+	})
 	$scope.title = 'Goals'
+	$scope.options = [
+		{id: 1, name: 'Month'},
+    {id: 2, name: 'Forthnight'},
+    {id: 3, name: 'Week'}
+	]
 	function listGoals(){
 		Goal.query(
 			function(goals){
@@ -34,4 +42,4 @@ var app = angular.module('monujo')
 app.controller('GoalsController', GoalsController)
 
 /*eslint no-console: ["error", { allow: ["log"] }] */
-/*global angular:true*/
+/*global angular $:true*/
